69bbf295e7e42943e36a7bc94819548e0b6a6288
Lior Fridman <76394602+liorspidy@users.noreply.github.com>
2023-05-02 15:54:36 +0300

    fixed logic in game and added words

--- src/components/game/WordleRow.js
+++ src/components/game/WordleRow.js
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useState } from 'react';
-import { AppContext } from '../../context/AppContext';
-import classes from './GameBoardStyle.module.css';
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import classes from "./GameBoardStyle.module.css";
 
 const WordleRow = ({ index }) => {
   const {
@@ -21,14 +21,16 @@ const WordleRow = ({ index }) => {
   const [almost, setAlmost] = useState({});
   const [wrong, setWrong] = useState({});
 
-  const items = currentWord.split('');
-  const foundWord = foundWords[index - 1]?.split('');
+  const items = currentWord.split("");
+  const foundWord = foundWords[index - 1]?.split("");
 
   useEffect(() => {
-    const pickedArray = pickedWord.split('');
+    const pickedArray = pickedWord.split("");
     let counter = 0;
+    const newCorrect = {};
     for (let i = 0; i < 5; i++) {
       if (foundWord && foundWord[i] === pickedArray[i]) {
+        newCorrect[i] = foundWord[i];
         setCorrectLetters((prevCorrect) => {
           if (!prevCorrect.hasOwnProperty(foundWord[i])) {
             return {
@@ -40,23 +42,37 @@ const WordleRow = ({ index }) => {
         });
         counter++;
         if (counter === 5) {
-          const timeout = setTimeout(() => {
+          setTimeout(() => {
             setGameState(1);
             setShowEndGame(true);
           }, 300);
         }
         setCorrect((prevCorrect) => ({ ...prevCorrect, [i]: foundWord[i] }));
-      } else if (foundWord && pickedArray.includes(foundWord[i])) {
+      }
+    }
+
+    const filteredArray = pickedArray.filter(
+      (letter, index) => !newCorrect[index] || newCorrect[index] !== letter
+    );
+
+    // find the remaining letters that are not in the correct position of the pickedWord
+    for (let i = 0; i < 5; i++) {
+      const letter = foundWord ? foundWord[i] : [];
+      if (
+        letter !== null &&
+        filteredArray.includes(letter) &&
+        !Object.keys(newCorrect).includes(i)
+      ) {
         setAlmostLetters((prevAlmost) => {
-          if (!prevAlmost.hasOwnProperty(foundWord[i])) {
+          if (!prevAlmost.hasOwnProperty(letter)) {
             return {
               ...prevAlmost,
-              [foundWord[i]]: foundWord[i],
+              [letter]: letter,
             };
           }
           return prevAlmost;
         });
-        setAlmost((prevAlmost) => ({ ...prevAlmost, [i]: foundWord[i] }));
+        setAlmost((prevAlmost) => ({ ...prevAlmost, [i]: letter }));
       } else {
         if (foundWord) {
           setWrongLetters((prevWrong) => {
@@ -96,14 +112,14 @@ const WordleRow = ({ index }) => {
             ? classes.almost
             : foundWord && wrong[4] === foundWord[4]
             ? classes.wrong
-            : ''
+            : ""
         }`}
       >
         {foundWords[index - 1]?.length
           ? foundWord[4]
           : currentRowIndex === +index
           ? items[4]
-          : ''}
+          : ""}
       </div>
       <div
         className={`${classes.wordleBlock} ${
@@ -113,14 +129,14 @@ const WordleRow = ({ index }) => {
             ? classes.almost
             : foundWord && wrong[3] === foundWord[3]
             ? classes.wrong
-            : ''
+            : ""
         }`}
       >
         {foundWords[index - 1]?.length
           ? foundWord[3]
           : currentRowIndex === +index
           ? items[3]
-          : ''}
+          : ""}
       </div>
       <div
         className={`${classes.wordleBlock} ${
@@ -130,14 +146,14 @@ const WordleRow = ({ index }) => {
             ? classes.almost
             : foundWord && wrong[2] === foundWord[2]
             ? classes.wrong
-            : ''
+            : ""
         }`}
       >
         {foundWords[index - 1]?.length
           ? foundWord[2]
           : currentRowIndex === +index
           ? items[2]
-          : ''}
+          : ""}
       </div>
       <div
         className={`${classes.wordleBlock} ${
@@ -147,14 +163,14 @@ const WordleRow = ({ index }) => {
             ? classes.almost
             : foundWord && wrong[1] === foundWord[1]
             ? classes.wrong
-            : ''
+            : ""
         }`}
       >
         {foundWords[index - 1]?.length
           ? foundWord[1]
           : currentRowIndex === +index
           ? items[1]
-          : ''}
+          : ""}
       </div>
       <div
         className={`${classes.wordleBlock} ${
@@ -164,14 +180,14 @@ const WordleRow = ({ index }) => {
             ? classes.almost
             : foundWord && wrong[0] === foundWord[0]
             ? classes.wrong
-            : ''
+            : ""
         }`}
       >
         {foundWords[index - 1]?.length
           ? foundWord[0]
           : currentRowIndex === +index
           ? items[0]
-          : ''}
+          : ""}
       </div>
     </div>
   );
